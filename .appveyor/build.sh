9590d3cfa8ae2d60417044b8a63d39d444e68ec7
Shao Cheng <astrohavoc@gmail.com>
2018-06-08 00:37:31 +0800

    Disable sphinx docs with appveyor

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/sh -e
 
-pacman -S --noconfirm --noprogressbar \
-    mingw-w64-x86_64-python2-sphinx
+pacman -S --needed --noconfirm --noprogressbar \
+    autoconf \
+    automake \
+    bsdtar \
+    git \
+    make \
+    mingw-w64-x86_64-ca-certificates \
+    mingw-w64-x86_64-curl \
+    mingw-w64-x86_64-gcc \
+    mingw-w64-x86_64-libtool \
+    mingw-w64-x86_64-python2 \
+    mingw-w64-x86_64-tools-git \
+    mingw-w64-x86_64-xz \
+    patch \
+    p7zip \
+    tar
 
 stack --resolver nightly --skip-msys setup > /dev/null
 stack --skip-msys install \
     alex \
     happy \
     hscolour
-export PATH=~/.local/bin:$(stack path --compiler-bin --skip-msys):$PATH
+export PATH=~/local/bin:$(stack path --compiler-bin --skip-msys):$PATH
 
 cd /tmp/.appveyor/ghc
 mv ../build.mk mk/
