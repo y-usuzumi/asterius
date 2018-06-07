755be50600c397bd90424dea3b81789716976852
Shao Cheng <astrohavoc@gmail.com>
2018-06-08 00:18:41 +0800

    Fix stack path

--- .appveyor/build.sh
+++ .appveyor/build.sh
@@ -1,14 +1,14 @@
-#!/bin/sh -e
+#!/usr/bin/sh -e
 
 pacman -S --noconfirm --noprogressbar \
     mingw-w64-x86_64-python2-sphinx
 
-mkdir -p ~/.local/bin
+stack --resolver nightly --skip-msys setup > /dev/null
 stack --skip-msys install \
     alex \
     happy \
     hscolour
-export PATH=~/.local/bin:$(stack path --compiler-bin):$PATH
+export PATH=~/.local/bin:$(stack path --compiler-bin --skip-msys):$PATH
 
 cd /tmp/.appveyor/ghc
 mv ../build.mk mk/
